79ca3380d18bc0dda738bf2c667c5f8b3ca86adb
Raghav <raghav.rajaraman@gmail.com>
2025-07-22 10:29:48 +0530

    Word Grids fix

--- src/app/components/common/common.ts
+++ src/app/components/common/common.ts
@@ -2,5 +2,6 @@ export const wordsGrid = [
   ["apple", "brave", "crisp", "daisy", "eagle", "flame", "grape"],
   ["honey", "ivory", "joker", "kneel", "lemon", "mango", "naval"],
   ["ocean", "piano", "quiet", "raven", "storm", "tiger", "uncle"],
-  ["vivid", "waltz", "xenon", "youth", "zebra", "blaze", "candy"]
+  ["vivid", "waltz", "xenon", "youth", "zebra", "blaze", "candy"],
+  ["vivid", "waltz", "xenon", "youth", "zebra", "blaze", "candy"],
 ];
